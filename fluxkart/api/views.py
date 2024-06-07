3b632516d3366c7260590d004ddb82a028cea308
rutvikkapade <rutvik609@gmail.com>
2024-06-07 13:29:18 +0530

    updated settings.py

--- fluxkart/api/views.py
+++ fluxkart/api/views.py
@@ -5,6 +5,8 @@ from .serializer import ContactSerializer
 from .swagger_schemas import create_contact_schema
 
 class ContactViewSet(viewsets.ViewSet):
+
+
     @create_contact_schema
     def create(self, request):
 
