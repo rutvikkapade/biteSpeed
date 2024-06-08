c865eb8cb8f69b328d0f02a3fd1fed99c22d2237
rutvikkapade <rutvik609@gmail.com>
2024-06-08 17:45:43 +0530

    some fixes

--- fluxkart/api/views.py
+++ fluxkart/api/views.py
@@ -4,23 +4,22 @@ from .models import Contact
 from .serializer import ContactSerializer
 from .swagger_schemas import create_contact_schema
 
-class ContactViewSet(viewsets.ViewSet):
 
+class ContactViewSet(viewsets.ViewSet):
 
     @create_contact_schema
     def create(self, request):
-
         """
         Create a new contact or link an existing contact based on the provided email and phone number.
-        
+
         If neither email nor phone number exist, a new primary contact is created.
         If either email or phone number exists, a new secondary contact is created and linked to the existing primary contact.
         If both email and phone number exist in the same row, no new contact is created.
-        
+
         Parameters:
             email (str): Email address of the contact
             phoneNumber (str): Phone number of the contact
-        
+
         Returns:
             Response: A response containing the primary contact details and linked secondary contacts.
         """
@@ -30,17 +29,20 @@ class ContactViewSet(viewsets.ViewSet):
             if not phoneNumber and not email:
                 return Response("Both Cant be Null", status=status.HTTP_400_BAD_REQUEST)
             # Check if there is any contact with the same email or phoneNumber
-            if not isinstance(email,str,None) and not isinstance(phoneNumber,int,None):
+            if not isinstance(email, str, None) and not isinstance(phoneNumber, int, None):
                 return Response("Invalid data types", status=status.HTTP_400_BAD_REQUEST)
             existing_contact_by_email = None
             existing_contact_by_phone = None
             if email:
-                existing_contact_by_email = Contact.objects.filter(email=email).first()
+                existing_contact_by_email = Contact.objects.filter(
+                    email=email).first()
             if phoneNumber:
-                existing_contact_by_phone = Contact.objects.filter(phoneNumber=phoneNumber).first()
+                existing_contact_by_phone = Contact.objects.filter(
+                    phoneNumber=phoneNumber).first()
             if existing_contact_by_email and existing_contact_by_phone:
                 # If both email and phone number exist in the same row, do not add any rows
-                print(existing_contact_by_email.id,"      ",existing_contact_by_phone.id)
+                print(existing_contact_by_email.id,
+                      "      ", existing_contact_by_phone.id)
                 if existing_contact_by_email.id == existing_contact_by_phone.id:
                     if existing_contact_by_email.linkPrecedence == 'secondary':
                         primary_contact = existing_contact_by_email.linkedId
@@ -49,24 +51,30 @@ class ContactViewSet(viewsets.ViewSet):
                     response_data = self.build_response(primary_contact)
                     return Response(response_data, status=status.HTTP_200_OK)
                 else:
-                    if existing_contact_by_email.id<existing_contact_by_phone.id:
+                    if existing_contact_by_email.id < existing_contact_by_phone.id:
                         existing_contact_by_phone.linkPrecedence = 'secondary'
                         if existing_contact_by_email.linkPrecedence == 'secondary':
                             primary_contact = existing_contact_by_email.linkedId
                         else:
                             primary_contact = existing_contact_by_email
-                        Contact.objects.filter(id = existing_contact_by_phone.linkedId.id).update(linkedId = primary_contact , linkPrecedence = 'secondary')
-                        Contact.objects.filter(linkedId = existing_contact_by_phone.linkedId).update(linkedId = primary_contact)
-                        Contact.objects.filter(linkedId=existing_contact_by_phone).update(linkedId=primary_contact)
+                        Contact.objects.filter(id=existing_contact_by_phone.linkedId.id).update(
+                            linkedId=primary_contact, linkPrecedence='secondary')
+                        Contact.objects.filter(linkedId=existing_contact_by_phone.linkedId).update(
+                            linkedId=primary_contact)
+                        Contact.objects.filter(linkedId=existing_contact_by_phone).update(
+                            linkedId=primary_contact)
                     else:
                         existing_contact_by_email.linkPrecedence = 'secondary'
                         if existing_contact_by_phone.linkPrecedence == 'secondary':
                             primary_contact = existing_contact_by_phone.linkedId
                         else:
                             primary_contact = existing_contact_by_phone
-                        Contact.objects.filter(id = existing_contact_by_email.linkedId.id).update(linkedId = primary_contact , linkPrecedence = 'secondary')
-                        Contact.objects.filter(linkedId = existing_contact_by_email.linkedId).update(linkedId = primary_contact)
-                        Contact.objects.filter(linkedId=existing_contact_by_email).update(linkedId=primary_contact)
+                        Contact.objects.filter(id=existing_contact_by_email.linkedId.id).update(
+                            linkedId=primary_contact, linkPrecedence='secondary')
+                        Contact.objects.filter(linkedId=existing_contact_by_email.linkedId).update(
+                            linkedId=primary_contact)
+                        Contact.objects.filter(linkedId=existing_contact_by_email).update(
+                            linkedId=primary_contact)
                     response_data = self.build_response(primary_contact)
                     return Response(response_data, status=status.HTTP_200_OK)
             if not existing_contact_by_email and not existing_contact_by_phone:
@@ -84,26 +92,30 @@ class ContactViewSet(viewsets.ViewSet):
                 primary_contact = existing_contact_by_email or existing_contact_by_phone
                 print(primary_contact)
                 if primary_contact.linkPrecedence == 'secondary':
-                    linkedId=primary_contact.linkedId
+                    linkedId = primary_contact.linkedId
                 else:
-                    linkedId=primary_contact
+                    linkedId = primary_contact
                 Contact.objects.create(
-                        email=email,
-                        phoneNumber=phoneNumber,
-                        linkedId = linkedId,
-                        linkPrecedence='secondary'
-                    )
+                    email=email,
+                    phoneNumber=phoneNumber,
+                    linkedId=linkedId,
+                    linkPrecedence='secondary'
+                )
             response_data = self.build_response(linkedId)
             return Response(response_data, status=status.HTTP_201_CREATED)
         except Exception as e:
-            Response('An Error Occured', status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-    
+            Response('An Error Occured',
+                     status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
     def build_response(self, primary_contact):
-        secondary_contacts = Contact.objects.filter(linkedId = primary_contact)
+        secondary_contacts = Contact.objects.filter(linkedId=primary_contact)
         emails = [primary_contact.email] if primary_contact.email else []
-        phoneNumbers = [primary_contact.phoneNumber] if primary_contact.phoneNumber else []
-        [emails.append(contact.email) for contact in secondary_contacts if contact.email not in emails and contact.email]
-        [phoneNumbers.append(contact.phoneNumber) for contact in secondary_contacts if contact.phoneNumber not in phoneNumbers and contact.phoneNumber]
+        phoneNumbers = [
+            primary_contact.phoneNumber] if primary_contact.phoneNumber else []
+        [emails.append(contact.email)
+         for contact in secondary_contacts if contact.email not in emails and contact.email]
+        [phoneNumbers.append(contact.phoneNumber)
+         for contact in secondary_contacts if contact.phoneNumber not in phoneNumbers and contact.phoneNumber]
         secondary_contact_ids = [contact.id for contact in secondary_contacts]
 
         response_data = {
